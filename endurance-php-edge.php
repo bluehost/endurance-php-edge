85e9b05cd277971e6932faacb699cb7a6d1acad7
Mike <web.mhansen@gmail.com>
2016-11-26 13:32:12 -0700

    Initial code to add the handler to the htaccess file.

---
+++ endurance-php-edge.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: Endurance PHP Edge
+Description: Run the latest stable version of PHP available on .
+Version: 0.1
+Author: Mike Hansen
+Author URI: https://www.mikehansen.me/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Do not access file directly!
+if ( ! defined( 'WPINC' ) ) { die; }
+
+define( 'EBC_VERSION', 0.1 );
+
+if ( ! class_exists( 'Endurance_PHP_Edge' ) ) {
+	class Endurance_PHP_Edge {
+		function __construct() {
+			$this->hooks();
+		}
+
+		function hooks() {
+			add_filter( 'mod_rewrite_rules', array( $this, 'htaccess_contents' ) );
+		}
+
+		function htaccess_contents( $rules ) {
+			$handler = 'AddHandler application/x-httpd-php-edge .php' . "\n";
+			return $handler . $rules;
+		}
+	}
+	$ebc = new Endurance_PHP_Edge;
+}
